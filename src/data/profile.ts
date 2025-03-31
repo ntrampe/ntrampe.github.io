0bc276b74869325cb8250135d358609801783e13
Kitsune Apps <kitsune.apps@gmail.com>
2025-03-31 17:35:26 -0500

    fix: using explicit img components rather than Astro's Image optimization component

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -11,17 +11,17 @@ export interface Profile {
 
 export const profile: Profile = {
   name: "Nicholas Trampe",
-  picture: "/src/assets/me.jpg",
+  picture: "me.jpg",
   description:
     "I like to build beautifully native mobile experiences, explore faraway places, and film every detail of my experiences 👨‍💻🏔🎥",
   social: [
     {
-      icon: "/src/assets/instagram-logo.svg",
+      icon: "instagram-logo.svg",
       url: "https://instagram.com/nicholas.eager",
       platform: "Instagram",
     },
     {
-      icon: "/src/assets/youtube-logo.svg",
+      icon: "youtube-logo.svg",
       url: "https://youtube.com/@NicholasEager",
       platform: "YouTube",
     },
