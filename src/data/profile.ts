9fe665fb0782f7f292bda3dbd7c386009231e30a
Kitsune Apps <kitsune.apps@gmail.com>
2025-03-31 16:22:48 -0500

    refactor: using data source for profile

---
+++ src/data/profile.ts
@@ -0,0 +1,29 @@
+export interface Profile {
+  name: string;
+  picture: string;
+  description: string;
+  social: {
+    platform: string;
+    url: string;
+    icon: string;
+  }[];
+}
+
+export const profile: Profile = {
+  name: "Nicholas Eager",
+  picture: "/src/assets/me.jpg",
+  description:
+    "I like to build beautifully native mobile experiences, explore faraway places, and film every detail of my experiences 👨‍💻🏔🎥",
+  social: [
+    {
+      icon: "/src/assets/instagram-logo.svg",
+      url: "https://instagram.com/nicholas.eager",
+      platform: "Instagram",
+    },
+    {
+      icon: "/src/assets/youtube-logo.svg",
+      url: "https://youtube.com/@NicholasEager",
+      platform: "YouTube",
+    },
+  ],
+};
