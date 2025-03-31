f5a5ea7370e0ece15f54e40e000643b168602e42
Kitsune Apps <kitsune.apps@gmail.com>
2025-03-31 17:12:57 -0500

    fix: using vite for tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
   experimental: {
     svg: true,
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
