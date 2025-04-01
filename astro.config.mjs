c0888827ccf81ff1cd134a607030819f3281e2d9
Kitsune Apps <kitsune.apps@gmail.com>
2025-04-01 11:41:05 -0500

    chore: adding site to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ntrampe.github.io",
   experimental: {
     svg: true,
   },
