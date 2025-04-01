b86a339e2b30b1eeb7eddbb6a397965dce48230d
Kitsune Apps <kitsune.apps@gmail.com>
2025-04-01 11:35:29 -0500

    chore: adding misc item

--- src/data/misc.ts
+++ src/data/misc.ts
@@ -1,3 +1,4 @@
 export const misc: string[] = [
+  "Using the Eleven Labs API to [automatically add David Attenborough style narration to my hiking videos](https://www.youtube.com/watch?v=Mp4H86Xw6SE)",
   "Download a PDF version of my [resume](https://drive.google.com/uc?export=download&id=1NoS95CCGDlaKg6QsYO5EM4dPwMEeLBM6)",
 ];
