a3bb0a2b5d2b62cd8a47d6ba6058b1f4a267b261
Nicholas Trampe <nicholas.trampe@gmail.com>
2026-03-05 16:21:25 -0700

    chore: update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://ntrampe.github.io",
-  experimental: {
-    svg: true,
-  },
 
   vite: {
     plugins: [tailwindcss()],
